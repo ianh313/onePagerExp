2637db6c44df6062a68ed3dc27223b372b0cac37
ianh313 <101088783+ianh313@users.noreply.github.com>
2026-04-10 12:51:43 +0800

    Add Apps Script URL

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // After deploying your Google Apps Script Web App,
 // paste the URL below (looks like https://script.google.com/macros/s/…/exec)
-export const SCRIPT_URL = 'PASTE_YOUR_APPS_SCRIPT_URL_HERE'
+export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwo2T7oaFRtebTHq2jMegSR4VdPD4WQ5fC2SEMf_cvpwUtLwqwuTgHlkWJsk1ktZuC5Qw/exec'
